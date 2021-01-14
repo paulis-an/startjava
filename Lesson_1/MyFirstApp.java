5423c79574c9ed12f8294dd2bd596960d7335384
Pavel Anisimov <anisimov@cdkino.ru>
2021-01-14 22:35:23 +0300

    HomeWork Lesson_1 v1.1

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -1,5 +1,5 @@
-public class MyFirstApp{
+public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Привет, world");
+        System.out.println("Hello, world");
     }
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,22 @@
 class Variable {
-    public static void main (String[] args){
-        byte num_core = 8;
-        short proc = 3400;
+    public static void main(String[] args) {
+        byte numCore = 8;
+        short cpu = 3400;
         int memory = 8000;
-        long l = 1253657L;
-        float f = 3.14F;
-        double proc2 = 3.40;
-        char c = '#';
+        long longNum = 1253657L;
+        float pi = 3.14F;
+        double cpu2 = 3.40;
+        char symbol = '#';
         boolean b = true;
-        System.out.println("Num_core = " + num_core);
-        System.out.println("Proc = " + proc);
-        System.out.println("Memory = " + memory);
-        System.out.println("Long var = " + l);
-        System.out.println("Processor = " + proc2);
-        System.out.println("Char = " + c);
-        System.out.println("Boolean var = " + b);
 
+        System.out.println("Num_core = " + numCore);
+        System.out.println("CPU = " + cpu + " MHz");
+        System.out.println("Memory = " + memory + " MHz" );
+        System.out.println("Processor = " + cpu2 + " GHz");
+        System.out.println("---------------------");
+        System.out.println("Long var = " + longNum);
+        System.out.println("Pi = " + pi);
+        System.out.println("Char = " + symbol);
+        System.out.println("Boolean var = " + b);
     }
 }
