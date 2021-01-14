0ccdc152c72164a0f3a4a3908af9a802e3a346af
Pavel Anisimov <anisimov@cdkino.ru>
2021-01-14 16:05:40 +0300

    HomeWork Lesson_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+class Variable {
+    public static void main (String[] args){
+        byte num_core = 8;
+        short proc = 3400;
+        int memory = 8000;
+        long l = 1253657L;
+        float f = 3.14F;
+        double proc2 = 3.40;
+        char c = '#';
+        boolean b = true;
+        System.out.println("Num_core = " + num_core);
+        System.out.println("Proc = " + proc);
+        System.out.println("Memory = " + memory);
+        System.out.println("Long var = " + l);
+        System.out.println("Processor = " + proc2);
+        System.out.println("Char = " + c);
+        System.out.println("Boolean var = " + b);
+
+    }
+}
