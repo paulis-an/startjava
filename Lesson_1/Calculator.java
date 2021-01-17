9a1b74aacb8d74e1ef1700a65f0a94fa8b8d893f
Pavel Anisimov <anisimov@cdkino.ru>
2021-01-17 19:14:26 +0300

    fixed Calculator v1.2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,28 +1,28 @@
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 10;
         int b = 4;
-        char c = '^';
+        char mathSign = '^';
         
-        if(c == '+') {
+        if(mathSign == '+') {
             System.out.println("Сумма чисел = " + (a + b));
-        } else if(c == '-') {
+        } else if(mathSign == '-') {
             System.out.println("Разность чисел = " + (a - b));
-        } else if(c == '*') {
+        } else if(mathSign == '*') {
             System.out.println("Произведение чисел = " + (a * b));
-        } else if(c == '/') {
+        } else if(mathSign == '/') {
             if(b != 0) {
                 System.out.println("Деление а на b = " + (a / b));
             } else {
                 System.out.println("Деление на 0 запрещено");
             }
-        } else if(c == '^') {
+        } else if(mathSign == '^') {
             int exp = a;
             if(b > 1) {
                 for(int i = 1; i < b ; i++) {
                     exp *= a;
                 }
-            System.out.println("Возведение в степень b числа а = " + exp);
+                System.out.println("Возведение в степень b числа а = " + exp);
             } else if(b == 1) {
                 System.out.println("Число в степени 1 равно самому себе и = " + a);
             } else {
