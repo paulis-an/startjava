39f8c461fc61b31b63bfcafe78c5a6478b2fef86
Pavel Anisimov <anisimov@cdkino.ru>
2021-01-26 16:47:53 +0300

    fixed GuessNumber v1.2

--- Lesson_2/GuessNumber/GuessNumberTest.java
+++ Lesson_2/GuessNumber/GuessNumberTest.java
@@ -4,7 +4,7 @@ public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String answer = "да";
-        while (answer.equals("да")) {
+        while(answer.equals("да")) {
             System.out.println("Введите имя игрока №1");
             Player player1 = new Player(scan.nextLine());
             System.out.println("Введите имя игрока №2");
@@ -15,7 +15,7 @@ public class GuessNumberTest {
             do {
                 System.out.println("Хотите продолжить игру? [да/нет]:");
                 answer = scan.next();
-            } while (!answer.equals("да") && !answer.equals("нет"));
+            } while(!answer.equals("да") && !answer.equals("нет"));
         }
     }
 }
